c127a667e9d47c91ac164d1ba2bee88d30931142
Jimish Ravat <jimishravat2802@gmail.com>
2026-03-28 15:56:45 +0530

    Update mail.js

--- config/mail.js
+++ config/mail.js
@@ -12,6 +12,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  family: 4, // Use IPv4
   // Connection pool for better performance
   pool: {
     maxConnections: 5,
